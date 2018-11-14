53a22d5c44951ab9968b4616084ed42a0c1be3e8
cuong.duong <cuong.duong@titancorpvn.com>
2018-11-14 14:16:00 +0700

    add tachyons & style 'Sign Out'/background page

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Navigation = () => {
-  return (
+  return ( // dim: hover
     <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-      <p>Sign Out</p>
+      <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
     </nav>
   )
 }
